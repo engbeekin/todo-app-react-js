85eda04be4ab3f2af416f83dcaa1c7c49c888dd8
engbeekin <54867568+engbeekin@users.noreply.github.com>
2022-04-16 19:31:31 +0100

    added todoform and todo filter by month

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import Todo from './components/Todo';
+import React, { useState } from 'react';
+import Todo from './components/Todos/Todo';
+import NewTodo from './components/NewTodo/NewTodo';
+
+const Dummy_Todo = [
+	{ id: 1, title: 'building React js project', date: new Date('2022-04-16') },
+	{ id: 2, title: 'Gym', date: new Date('2022-04-10') },
+	{ id: 3, title: 'reading book', date: new Date('2022-03-1') },
+	{ id: 4, title: 'family time', date: new Date('2022-02-27') }
+];
 function App() {
-	const Dummy_Todo = [
-		{ id: 1, title: 'building React js project', date: new Date('2022-04-16') },
-		{ id: 2, title: 'Gym', date: new Date('2022-04-10') },
-		{ id: 3, title: 'reading book', date: new Date('2022-03-1') },
-		{ id: 4, title: 'family time', date: new Date('2022-02-27') }
-	];
+	const [ todos, setTodos ] = useState(Dummy_Todo);
+
+	const addedNewTodoDataHandler = (newTodo) => {
+		setTodos((todos) => [ newTodo, ...todos ]);
+	};
+
 	return (
-		<div className="App">
-			<Todo items={Dummy_Todo} />
+		<div className="App container">
+			<NewTodo onAddeNewTodoData={addedNewTodoDataHandler} />
+			<Todo items={todos} />
 		</div>
 	);
 }
