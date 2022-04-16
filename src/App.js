be48cfd2e24600529e3b4e081625fd4a7a9d54cb
engbeekin <54867568+engbeekin@users.noreply.github.com>
2022-04-16 15:37:40 +0100

    added dummy todo list and todo componet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import Todo from './components/Todo';
 function App() {
+	const Dummy_Todo = [
+		{ id: 1, title: 'building React js project', date: new Date('2022-04-16') },
+		{ id: 2, title: 'Gym', date: new Date('2022-04-10') },
+		{ id: 3, title: 'reading book', date: new Date('2022-03-1') },
+		{ id: 4, title: 'family time', date: new Date('2022-02-27') }
+	];
 	return (
 		<div className="App">
-			
+			<Todo items={Dummy_Todo} />
 		</div>
 	);
 }
